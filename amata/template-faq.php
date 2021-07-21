f8a1b30113be245795ad3103276d3a44df9c0b58
Lenka <lenkazivkovic24@gmail.com>
2021-07-21 11:43:22 +0200

    faq page - done + pricing - beggin

--- amata/template-faq.php
+++ amata/template-faq.php
@@ -6,45 +6,27 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-lg-8 text-center mar-auto">
-					<h2>Frequently Asked Questions</h2>
+					<h2><?php the_field('heading'); ?> <div class="after-center"></div></h2>
 				</div>
 				<div class="col-lg-8 mar-auto">
 					<div class="acc-kontainer">
-						<div>
-							<input type="radio" name="acc" id="acc1" checked>
-							<label for="acc1"><i class="ri-add-line"></i> Question 1</label>
-							<div class="acc-body">
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lectus commodo egestas congue quis volutpat. Arcu fringilla ultricies velit pellentesque facilisis. Sagittis, fermentum proin tincidunt sit interdum pulvinar fermentum vulputate.
-							</div>
-						</div>
-						<div>
-							<input type="radio" name="acc" id="acc2">
-							<label for="acc2"><i class="ri-add-line"></i> Question 2</label>
-							<div class="acc-body">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lectus commodo egestas congue quis volutpat. Arcu fringilla ultricies velit pellentesque facilisis. Sagittis, fermentum proin tincidunt sit interdum pulvinar fermentum vulputate.
-							</div>
-						</div>
-						<div>
-							<input type="radio" name="acc" id="acc3">
-							<label for="acc3"><i class="ri-add-line"></i> Question 3</label>
-							<div class="acc-body">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lectus commodo egestas congue quis volutpat. Arcu fringilla ultricies velit pellentesque facilisis. Sagittis, fermentum proin tincidunt sit interdum pulvinar fermentum vulputate.
-							</div>
-						</div>
-						<div>
-							<input type="radio" name="acc" id="acc4">
-							<label for="acc4"><i class="ri-add-line"></i> Question 4</label>
-							<div class="acc-body">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lectus commodo egestas congue quis volutpat. Arcu fringilla ultricies velit pellentesque facilisis. Sagittis, fermentum proin tincidunt sit interdum pulvinar fermentum vulputate.
-							</div>
-						</div>
-						<div>
-							<input type="radio" name="acc" id="acc5">
-							<label for="acc5"><i class="ri-add-line"></i> Question 5</label>
-							<div class="acc-body">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lectus commodo egestas congue quis volutpat. Arcu fringilla ultricies velit pellentesque facilisis. Sagittis, fermentum proin tincidunt sit interdum pulvinar fermentum vulputate.
-							</div>
-						</div>
+						<?php
+						if (have_rows('faq_accordion')) :
+							while (have_rows('faq_accordion')) : the_row();
+						?>
+								<div>
+									<input type="radio" name="acc" id="<?php echo the_sub_field('id');?>">
+									<label for="<?php echo the_sub_field('id');?>"><i class="ri-add-line"></i> <?php the_sub_field('question');?></label>
+									<div class="acc-body">
+										<?php the_sub_field('answer');?>
+									</div>
+								</div>
+						<?php
+							endwhile;
+						else :
+							var_dump(have_rows('faq_accordion'));
+						endif;
+						?>
 					</div>
 				</div>
 			</div>
---
+++ amata/template-pricing.php
@@ -0,0 +1,31 @@
+<?php /* Template Name: Pricing Page Template */ get_header(); ?>
+
+<main role="main">
+	<!-- section -->
+	<section class="section">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 text-center mar-auto">
+					<h2>Pricing <div class="after-center"></div></h2>
+					<h3 class="text-center">Here’s an overview of our services, but if you want a detailed explanation, just visit this link.</h3>
+				</div>
+				<div class="col-lg-10 mar-auto">
+					<div>
+						<div class="">
+							<h3>Order Management System</h3>
+							<p>track and transfer delivery information in the OMS.</p>
+						</div>
+						<div>
+							<p>Inquire</p>
+							<i class="ri-arrow-right-s-fill"></i>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- /section -->
+
+</main>
+
+<?php get_footer(); ?>
